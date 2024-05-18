e77af8d4638a7db69cb9f4efb6f64d67c1178348
Andrey Mangust <invoker2000.0606@gmail.com>
2024-05-18 19:31:29 +0200

    Added comments in Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,21 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Список выполненных команд:" +
+                "\ngit init" +
+                "\n git add ." +
+                "\ngit commit -m \"First adding\"" +
+                "\n git branch blog" +
+                "\n git checkout blog" +
+                "\ngit remote add origin https://github.com/AndreiKorobeinikov/Module5test.git" +
+                "\ngit add ." +
+                "\ngit commit -m \"blog added\"" +//Да, это было не нужно, но для чистоты эксперимента
+                "\ngit checkout master" +
+                "\ngit push -u origin master" +
+                "\n СПИСОК .gitignore" +
+                "\n *.sass\n" +
+                " bin/\n" +
+                " admin/\n" +
+                " config/");
     }
 }
